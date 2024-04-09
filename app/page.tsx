88d8df47d4ea64b00a1dc579840e2c4139b00526
ashwinsingh2007 <legend.ashwini07@gmail.com>
2024-04-09 12:49:39 +0530

    lint fix

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         </div>
 
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          <span className="text-blue-600">Unlock</span> ChatGPT's power on any webpage
+          <span className="text-blue-600">Unlock</span> ChatGPT&apos;s power on any webpage
         </h1>
 
         <p className="mt-5 max-w-prose text-lg text-zinc-700 sm:text-2xl">
